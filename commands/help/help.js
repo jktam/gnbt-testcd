16d0cc7562c92a6a35106f364a88071e87a8530e
James Tam <james.kit.tam@gmail.com>
2019-05-20 01:31:19 -0700

    testing pipeline on azure

--- commands/help/help.js
+++ commands/help/help.js
@@ -1,6 +1,6 @@
 const help = () => {
 return `\`\`\`markdown
-List of Commands:\n* $track\n* $chow\n* $whoami\n* $test\`\`\``;
+List of Commands:\n* $track\n* $chow\n* $whoami\n* $test\*n JAMES EDIT TEST PIPELINE\`\`\``;
 }
 
 module.exports = help;
